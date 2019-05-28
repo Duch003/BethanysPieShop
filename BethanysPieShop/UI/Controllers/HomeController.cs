45cea4436f0a0588a9db72c6ed197549ab793831
Duch003 <tomaszmankin003@gmail.com>
2019-05-28 14:18:50 +0200

    Initial commit with few lessons done

---
+++ BethanysPieShop/UI/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UI.Models;
+using UI.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository repository;
+
+        // GET: /<controller>/
+        public HomeController(IPieRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel();
+            homeViewModel.Title = "Welcome to Bethany's Pie Shop!";
+            homeViewModel.Pies = repository.GetAllPies().OrderBy(item => item.Name).ToList();
+
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ BethanysPieShop/UI/ViewModels/HomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UI.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UI.ViewModels
+{
+    public class HomeViewModel : Controller
+    {
+        public string Title { get; set; }
+        public List<Pie> Pies { get; set; }
+    }
+}
